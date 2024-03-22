74d719fa6fe721fa70bd67034899eae7ee129ddf
kellengerla <kellen.gerla@ucalgary.ca>
2024-03-21 19:01:29 -0600

    Added some more color to MatrixOperatorsDemo

--- src/main/java/MatrixOperatorsDemo.java
+++ src/main/java/MatrixOperatorsDemo.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 public class MatrixOperatorsDemo {
     public static void main(String[] args) throws FileNotFoundException {
         String blueColor = "\u001B[34m";
+        String yellowColor = "\u001B[33;1m";
         String resetColor = "\u001B[0m";
 
         System.out.print("""
@@ -11,11 +12,11 @@ public class MatrixOperatorsDemo {
                 \n""");
         Matrix data = new Matrix(args[0]);
 
-        System.out.println("Original Matrix:");
+        System.out.println(STR."\{yellowColor}Original Matrix:\{yellowColor}");
         data.showMat();
         System.out.println();
 
-        System.out.println("Important Info:");
+        System.out.println(STR."\{yellowColor}Important Info:\{yellowColor}");
         System.out.println(STR."\{blueColor}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\{resetColor}");
         System.out.println("|  Determinant\t|\t [Length, Width] \t|\tSquare Matrix?\t|\t\tType?\t\t|");
         System.out.println("---------------------------------------------------------------------------------");
@@ -29,17 +30,17 @@ public class MatrixOperatorsDemo {
         System.out.println(STR."\t\t\{data.getType()}\t\t|");
         System.out.println(STR."\{blueColor}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\{resetColor}");
         System.out.println();
-        System.out.println("Transposed Matrix:");
+        System.out.println(STR."\{yellowColor}Transposed Matrix:\{yellowColor}");
         Matrix transposedData = Matrix.transpose(data);
         transposedData.showMat();
         System.out.println();
 
-        System.out.println("Inverse Matrix:");
+        System.out.println(STR."\{yellowColor}Inverse Matrix:\{yellowColor}");
         Matrix invertedData = Matrix.inverse(data);
         invertedData.showMat();
         System.out.println();
 
-        System.out.println("Multiplied Matrix of Original Matrix and Inverse Matrix:");
+        System.out.println(STR."\{yellowColor}Multiplied Matrix of Original Matrix and Inverse Matrix:\{yellowColor}");
         Matrix multipliedMatrix = Matrix.matMult(data, invertedData);
         multipliedMatrix.showMat();
 
